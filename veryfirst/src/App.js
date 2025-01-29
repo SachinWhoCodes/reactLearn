d7f30e650887609a8e97441a170cef97e9dc111c
Sachin Verma <ramasyasachin@gmail.com>
2025-01-29 23:58:23 +0530

    hello

---
+++ veryfirst/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Sita Ram</h1>
+  );
+}
+
+export default App;
---
+++ veryfirstbyvite/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+      <div>
+        <p>Hello Bhai</p>
+        <h1>Bolo Jai Shri Ram</h1>
+      </div>
+
+  )
+}
+
+export default App
